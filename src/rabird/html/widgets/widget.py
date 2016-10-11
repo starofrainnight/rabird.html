9a335d8a0e76e16c07f44ebd9e5586c0d020878f
Hong-She Liang <starofrainnight@gmail.com>
2016-10-11 08:20:42 +0800

    Split each wrapper as a class
    So that we could implement some unify features not only xpath

--- src/rabird/html/widgets/widget.py
+++ src/rabird/html/widgets/widget.py
@@ -1,27 +1,50 @@
-
-class ElementXPathWrapper(object):
+class LxmlElementWrapper(object):
     def __init__(self, element):
         self._element = element
 
     def xpath(self, pattern):
-        if hasattr(self._element, "xpath"):
-            # Support lxml elements
-            return self._element.xpath(pattern)
-        elif hasattr(self._element, "xpath_find_all"):
-            # Support rabird.selenium elements
-            return self._element.xpath_find_all(pattern)
-        elif hasattr(self._element, "findall"):
-            # Support python ElementTree elements
-            return self._element.findall(pattern)
-        elif hasattr(self._element, "find_elements_by_xpath"):
-            # Support selenium elements
-            return self._element.find_elements_by_xpath(pattern)
-        else:
-            raise NotImplementedError("Unsupported element type!")
+        return self._element.xpath(pattern)
+
+class RabirdSeleniumElementWrapper(object):
+    def __init__(self, element):
+        self._element = element
+
+    def xpath(self, pattern):
+        return self._element.xpath_find_all(pattern)
+
+class ElementTreeElementWrapper(object):
+    def __init__(self, element):
+        self._element = element
+
+    def xpath(self, pattern):
+        return self._element.findall(pattern)
+
+class SeleniumElementWrapper(object):
+    def __init__(self, element):
+        self._element = element
+
+    def xpath(self, pattern):
+        return self._element.find_elements_by_xpath(pattern)
+
+def createElementWrapper(element):
+    if hasattr(element, "xpath"):
+        # Support lxml elements
+        return LxmlElementWrapper(element)
+    elif hasattr(element, "xpath_find_all"):
+        # Support rabird.selenium elements
+        return RabirdSeleniumElementWrapper(element)
+    elif hasattr(element, "findall"):
+        # Support python ElementTree elements
+        return ElementTreeElementWrapper(element)
+    elif hasattr(element, "find_elements_by_xpath"):
+        # Support selenium elements
+        return SeleniumElementWrapper(element)
+    else:
+        raise NotImplementedError("Unsupported element type!")
 
 class Widget(object):
     def __init__(self, element):
-        self._wrapper = ElementXPathWrapper(element)
+        self._wrapper = createElementWrapper(element)
 
     @property
     def element(self):
